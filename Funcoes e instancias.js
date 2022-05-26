8b44e89ca17ab4634fc5af5059f4a73574049455
Juan Victor de Souza Silva <79611143+JuanVictordeSouzaSilva@users.noreply.github.com>
2022-05-26 00:49:52 -0300

    Add files via upload

---
+++ Funcoes e instancias.js
@@ -0,0 +1,10 @@
+function pessoa() {
+    this.nome = "juan";
+    this.nome2 = "victor"
+    this.nome3 = "souza"
+}
+
+var pessoa = new pessoa();
+console.log(pessoa.nome);
+console.log(pessoa.nome2);
+console.log(pessoa.nome3);
---
+++ Objetos.js
@@ -0,0 +1,19 @@
+var pessoas = [{
+    'nome': 'juan',
+    'idade': 21,
+    'profissao':'programador'
+},
+{
+    'nome': 'victor',
+    'idade': 21,
+    'profissao':'programador'
+},];
+
+console.log(pessoas[0].nome);
+
+var noticias = [{
+    'titulo': 'noticia 1',
+    'conteudo':'seria o texto da noticia'
+},];
+
+console.log(noticias[0].titulo);
